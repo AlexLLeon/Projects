31af4396997eb194dc86aa7f945b600435e65b51
Alex <133220541+AlexLLeon@users.noreply.github.com>
2023-05-12 13:06:22 +0100

    TheNetherIsle.py

--- TheNetherIsle.py
+++ TheNetherIsle.py
@@ -1,55 +1,111 @@
 # What do you want to be known by?
+class Player:
 
-def player_intro():
-    print("Welcome to The Nether Isle!")
+    player_name = ''
+    player_class = None
+    player_weapon = ''
 
-    player = input(str("What is your name, adventurer? \n"))
-    print(f"Ah good to meet you, {player}.")
-  
-    character_class = get_class()
-    print(f"{character_class} is a mighty fine choice!")
+    def intro(self):
+        print("Welcome to The Nether Isle!")
+        self.get_name()
+        self.get_class()
+        self.player_weapon = self.player_class.get_weapon()
+        self.print_summary()
 
-    weapon = get_weapon()
+    def get_name(self):
+        self.player_name = input(str("What is your name, adventurer? \n"))
+        print(f"Ah good to meet you, {self.player_name}.")
 
-    print (f"So you're a {weapon} wielding {character_class}! Welcome to the Nether Isle, {player}.")
- 
+    def get_class(self): 
+        res = input(str("What class are you, traveller? \n[1] Barbarian \n[2] Wizard \n[3] Rogue \n[4] Ranger \n"))
+        if res == "1":
+            self.player_class = Barbarian()
+        elif res == "2":
+            self.player_class = Wizard()
+        elif res == "3":
+            self.player_class = Rogue()
+        elif res == "4":
+            self.player_class = Ranger()
+        else:
+            print("Sorry, I didn't understand your input. Please respond with 1, 2, 3 or 4.")
+            self.get_class()
 
-# What class?
+    def print_summary(self):
+        print(f"So you're a {self.player_weapon} wielding {self.player_class.name()}! Welcome to the Nether Isle, {self.player_name}.")
 
-def get_class():
-    res = input(str("What class are you, traveller? \n[1] Barbarian \n[2] Wizard \n[3] Rogue \n[4] Ranger \n"))
 
-    if res == "1":
+class Barbarian:
+
+    def name(self):
         return "Barbarian"
-    elif res == "2":
+
+    def get_weapon(self):
+        res = input(str("What is your weapon of choice, barbarian? \n[1] Sword \n[2] Axe \n[3] Mace \n"))
+
+        if res == "1":
+            return "sword"
+        elif res == "2":
+            return "axe"
+        elif res == "3":
+            return "mace"
+        else:
+            print("Sorry, I didn't understand your input. Please respond with 1, 2 or 3.")
+            return self.get_weapon()
+
+
+class Wizard:
+
+    def name(self):
         return "Wizard"
-    elif res == "3":
-        return "Rogue"
-    elif res == "4":
-        return "Ranger"
-    else:
-        return "Sorry, I didn't understand your input. Please respond with 1, 2, 3 or 4."
-
-      
-# What weaapon?
-      
-def get_weapon():
-    res = input(str("What weapon will you be using? \n[1] Sword \n[2] Axe \n[3] Staff \n[4] Bow \n[5] Dagger \n[6] Crossbow \n"))
-
-    if res == "1":
-        return "Sword"
-    elif res == "2":
-        return "Axe"
-    elif res == "3":
-        return "Staff"
-    elif res == "4":
-        return "Bow"
-    elif res == "5":
-        return "Dagger"
-    elif res == "6":
-        return "Crossbow"
-    else:
-        return "Sorry, I didn't understand your input. Please respond with 1, 2, 3, 4, 5 or 6."
     
+    def get_weapon(self):
+        res = input(str("What is your weapon of choice, wizard? \n[1] Staff \n[2] Wand \n[3] Spellbook \n"))
 
-player_intro()
+        if res == "1":
+            return "staff"
+        elif res == "2":
+            return "wand"
+        elif res == "3":
+            return "spellbook"
+        else:
+            print("Sorry, I didn't understand your input. Please respond with 1, 2 or 3.")
+            return self.get_weapon()
+        
+class Rogue:
+
+    def name(self):
+        return "Rogue"
+    
+    def get_weapon(self):
+        res = input(str("What is your weapon of choice, rogue? \n[1] Dagger \n[2] Crossbow \n[3] Throwing Stars \n"))
+
+        if res == "1":
+            return "Dagger"
+        elif res == "2":
+            return "Crossbow"
+        elif res == "3":
+            return "Throwing Stars"
+        else:
+            print("Sorry, I didn't understand your input. Please respond with 1, 2 or 3.")
+            return self.get_weapon()
+        
+class Ranger:
+
+    def name(self):
+        return "Ranger"
+    
+    def get_weapon(self):
+        res = input(str("What is your weapon of choice, ranger? \n[1] Long Bow \n[2] Spear \n[3] Darts \n"))
+                
+        if res == "1":
+            return "Long Bow"
+        elif res == "2":
+            return "Spear"
+        elif res == "3":
+            return "Darts"
+        else:
+            print("Sorry, I didn't understand your input. Please respond with 1, 2 or 3.")
+            return self.get_weapon()
+
+player = Player()
+player.intro()
